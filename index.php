2ebb63e6d5a617342c2b77980af86aaa1f3c492a
vasily <haldlugal@mail.ru>
2019-05-13 17:51:19 +0700

    First try!

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+echo "test<br>";
+echo "something <br>";
+$test = array('CodeIgniter', 'php', 'phpMyAdmin', 'www.lucidar.me'); 
+var_dump($test);
+echo $test[7];
+throw new Exception("test e");
+?>
